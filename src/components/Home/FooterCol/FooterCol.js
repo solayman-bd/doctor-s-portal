fb856616c85c3dac764c18cb842b03838a06b6e9
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-04-14 03:49:17 +0600

    homePageDone

--- src/components/Home/FooterCol/FooterCol.js
+++ src/components/Home/FooterCol/FooterCol.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const FooterCol = (props) => {
+  const menuItem = props.menuItems;
+  const menuTitle = props.menuTitle;
   return (
     <div className="col-md-3">
-      <h6 className="text-primary">
-        {props.menuTitle ? props.menuTitle : " "}
-      </h6>
+      <h6 className="text-primary">{menuTitle ? props.menuTitle : " "}</h6>
       <ul className="list-unstyled mt-4">
-        {props.menuItems.map((item, index) => (
+        {menuItem.map((item, index) => (
           <li key={index}>
             <Link
               to={item.link}
